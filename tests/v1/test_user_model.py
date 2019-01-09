b1f5fb1cdc935d9f11c2d3e5f519d9d37d9f8f03
Bakley <barclaykoin@gmail.com>
2019-01-09 12:24:14 +0300

    [features #163046467]add more tests

--- tests/v1/test_user_model.py
+++ tests/v1/test_user_model.py
@@ -2,7 +2,7 @@
 import json
 
 from . import BaseClassTest
-# from app.api.v1.models import users_models
+from app.auth.v1.models import database, User
 
 # user_data = user_models.UserModels()
 
@@ -10,33 +10,36 @@ from . import BaseClassTest
 class TestUserModels(BaseClassTest):
     """Test case for the user model"""
 
-    # def test_if_can_save_a_user(self):
-    #     """Test if a user is added to the data structure"""
-    #     user = self.user_data.create_user()
-    #     self.assertEqual(1, len(user_database))
-    #     self.assertTrue(user, list)
+    def test_if_can_save_a_user(self):
+        """Test if a user is added to the data structure"""
+        user = self.user1.save_user()
+        self.assertEqual(1, len(database.users))
+        self.assertTrue(user, dict)
+
+    def test_can_delete_user(self):
+        """Test successful deletion of user"""
+        self.user1.save_user()
+        self.assertEqual(1, len(database.users))
+        user = User.get_user_by_id(id=1)
+        user.delete_user()
+        self.assertEqual(0, len(database.users))
 
     def test_signup(self):
         """"Test if a user can signup"""
-        response = self.client.post('/api/v1/auth/user/register',
+        response = self.client.post('/auth/v1/register',
                                     data=json.dumps(self.user_data),
                                     content_type='application/json')
-        result = json.loads(response.data.decode('UTF-8'))
         self.assertEqual(response.status_code, 201)
-        self.assertTrue(result["message"], "Successfully registered")
-        self.assertNotEqual(response.status_code, 404)
 
     def test_login(self):
         """Test if a registered user can login"""
-        self.client.post('/api/v1/auth/user/register',
+        self.client.post('/api/v1/auth/register',
                          data=json.dumps(self.user_data),
                          content_type='application/json')
-        response = self.client.post('/api/v1/auth/user/login',
+        response = self.client.post('/auth/v1/login',
                                     data=json.dumps({
                                         "email": "barclay@koin.com",
                                         "password": "Hello@254"
                                     }))
 
-        result = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(result["message"], "Successfully logged in")

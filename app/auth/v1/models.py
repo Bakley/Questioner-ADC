5c852b54a2d35cd213f46ca9f68f5a50348243c5
Bakley <barclaykoin@gmail.com>
2019-01-09 10:22:36 +0300

    [features #163046467]add more functionality to the user model and JWT token generation

--- app/auth/v1/models.py
+++ app/auth/v1/models.py
@@ -50,11 +50,65 @@ class User():
     def save_admin(self):
         """Method to register an Admin"""
         setattr(self, 'isAdmin', True)
+        setattr(self, 'id', database.user_count + 1)
         database.users.update({self.isAdmin: self})
-        return self.vie
+        database.users.update({self.id: self})
+        database.user_count += 1
+        return self.view
 
     def view(self):
         """Method to jsonify user objects"""
         keys = ["firstname", "lastname", "othername",
                 "email", "phoneNumber", "username"]
         return {key: getattr(self, keys) for key in keys}
+
+    def validate_password(self, password):
+        """Method for validating password"""
+        if check_password_hash(self.password, password):
+            return True
+        return False
+
+    def delete_user(self):
+        """Method for deleting a user"""
+        del database.users[self.id]
+
+    @classmethod
+    def get_user_by_id(cls, id):
+        """Method for getting user by id"""
+        user = database.users.get(id)
+        if not user:
+            return {'message': 'User does not exist.'}
+        return user
+
+    @classmethod
+    def get_user_by_email(cls, email):
+        """Method for getting user by email"""
+        for id_ in database.users:
+            user = database.users.get(id_)
+            if user.email == email:
+                return user
+        return None
+
+    def generate_token(self):
+        """Method for generating token upon login"""
+        try:
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=5),
+                'iat': datetime.utcnow(),
+                'id': self.id
+            }
+            token = jwt.encode(
+                payload,
+                current_app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+            return token
+        except Exception as err:
+            return str(err)
+
+    @staticmethod
+    def decode_token(token):
+        """Method for decoding token generated"""
+        payload = jwt.decode(token, str(
+            current_app.config.get('SECRET')), algorithms=['HS256'])
+        return payload

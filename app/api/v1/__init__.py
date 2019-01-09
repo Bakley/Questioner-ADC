b5446ef565739bbd169b2ce49df34999293037c3
Bakley <barclaykoin@gmail.com>
2019-01-09 21:55:49 +0300

    [features #163046790]registered URL

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -7,4 +7,4 @@ version_1 = Blueprint('version1', __name__, url_prefix='/api/v1')
 api = Api(version_1)
 
 
-api.add_resource(MeetupResource, '/meetups', '/meetups/upcoming')
+api.add_resource(MeetupResource, '/meetups', '/meetups/<int:meetup_id>')

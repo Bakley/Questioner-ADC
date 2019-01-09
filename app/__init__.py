378d0e6aa17be4f7eeebd8afea8b5d9768654e54
Bakley <barclaykoin@gmail.com>
2019-01-09 20:52:51 +0300

    [features #163046681]register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 import os
 from flask import Flask
 from app.auth.v1 import version_1auth
+from app.api.v1 import version_1
 
 from config import app_config
 
@@ -11,7 +12,9 @@ def create_app(config_name):
     app = Flask(__name__)
     app.url_map.strict_slashes = False
     app.config.from_object(app_config[config_name])
+    app.config['BUNDLE_ERRORS'] = True
 
     app.register_blueprint(version_1auth)
+    app.register_blueprint(version_1)
 
     return app

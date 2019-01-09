0dbc7072ee81853625bb3c997eef2d02020f9f3d
Bakley <barclaykoin@gmail.com>
2019-01-09 12:22:13 +0300

    [features #163046467]register auth blueprint

--- app/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-from app.auth.views import SignupResource
-
-auth = Blueprint('authV1', __name__, url_prefix='/auth/v1')
-
-api = Api(auth)
-
-api.add_resource(SignupResource, '/register')

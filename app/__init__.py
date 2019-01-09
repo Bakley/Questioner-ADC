a820f12f940b25124934fa8ae04e6f7d36a7e7b6
Bakley <barclaykoin@gmail.com>
2019-01-09 12:21:50 +0300

    [features #163046467]register auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 """Initialize app."""
 import os
 from flask import Flask
+from app.auth.v1 import version_1auth
 
 from config import app_config
 
@@ -10,5 +11,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.url_map.strict_slashes = False
     app.config.from_object(app_config[config_name])
+
+    app.register_blueprint(version_1auth)
 
     return app

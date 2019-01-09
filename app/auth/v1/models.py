35bf43f4299cb090c35c7f4ef6c3ebae77c34f5b
Bakley <barclaykoin@gmail.com>
2019-01-09 09:59:25 +0300

    [features #163046467]able to create the user and admin records

--- app/auth/v1/models.py
+++ app/auth/v1/models.py
@@ -1,4 +1,4 @@
-from datatime import datatime, timedelta
+from datetime import datetime, timedelta
 from flask import current_app
 import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,11 +12,49 @@ class MockDb():
         self.users = {}
         self.meetups = {}
         self.questions = {}
-        self.comments = {}
-        self.rspvs = {}
+        self.user_count = 0
+        self.meetups_count = 0
+        self.questions_count = 0
 
     def drop(self):
         self.__init__()
 
 
 database = MockDb()
+
+
+class User():
+    """User class model"""
+
+    def __init__(self, firstname, lastname, othername, email,
+                 phoneNumber, username, password):
+        """Initializes the user details"""
+        self.id = None
+        self.firstname = firstname
+        self.lastname = lastname
+        self.othername = othername
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.username = username
+        self.password = generate_password_hash(password)
+        self.registered = datetime.utcnow().isoformat()
+        self.isAdmin = False
+
+    def save_user(self):
+        """Method to save user to the database"""
+        setattr(self, 'id', database.user_count + 1)
+        database.users.update({self.id: self})
+        database.user_count += 1
+        return self.view
+
+    def save_admin(self):
+        """Method to register an Admin"""
+        setattr(self, 'isAdmin', True)
+        database.users.update({self.isAdmin: self})
+        return self.vie
+
+    def view(self):
+        """Method to jsonify user objects"""
+        keys = ["firstname", "lastname", "othername",
+                "email", "phoneNumber", "username"]
+        return {key: getattr(self, keys) for key in keys}

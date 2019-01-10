85c2b9049b51fe29e2744da2d33f1a663adf7c6f
Bakley <barclaykoin@gmail.com>
2019-01-10 08:30:01 +0300

    [features #163047100]register the url

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 from app.api.v1.views.meetups_views import AllMeetupResource, MeetupResource
 from app.api.v1.views.questions_views import QuestionResource
+from app.api.v1.views.rspvs_views import RspvsResource
 
 
 version_1 = Blueprint('version1', __name__, url_prefix='/api/v1')
@@ -9,6 +10,7 @@ version_1 = Blueprint('version1', __name__, url_prefix='/api/v1')
 api = Api(version_1)
 
 
-api.add_resource(MeetupResource, '/meetups', '/meetups/<int:meetup_id>')
-api.add_resource(AllMeetupResource, '/meetups/upcoming')
+api.add_resource(MeetupResource, '/meetups', '/meetups/upcoming')
+api.add_resource(AllMeetupResource, '/meetups/<int:meetup_id>', '/meetups/<int:meetup_id>/rspv' )
 api.add_resource(QuestionResource, '/questions')
+api.add_resource(RspvsResource, '/meetups/<int:meetup_id>/rspvs')

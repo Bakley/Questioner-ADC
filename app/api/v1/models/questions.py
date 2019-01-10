fff5f741427a21ad7bbcd11a5d34fd269d47ba14
Bakley <barclaykoin@gmail.com>
2019-01-10 07:42:44 +0300

    [finishes #163046732]build the question models

--- app/api/v1/models/questions.py
+++ app/api/v1/models/questions.py
@@ -1,5 +1,5 @@
 """Meetup models"""
-
+from app.api.v1.models.meetups import meetup_models
 from datetime import datetime
 
 questions = []
@@ -12,13 +12,16 @@ class QuestionsModel:
         """Initializes the model"""
         self.db = questions
 
-    def create_meetup(self, body, title, tags):
-        """Method to save meetup records"""
+    def create_question(self, body, title, meetup, createdBy):
+        """Method to save question records"""
+
         payload = {
             "id": len(self.db) + 1,
             "body": body,
             "title": title,
             "votes": 0,
+            "meetup": meetup,
+            "createdBy": createdBy,
             "createdOn": datetime.utcnow().isoformat()
         }
         self.db.append(payload)
@@ -30,6 +33,6 @@ class QuestionsModel:
 
     def get_a_specific_question(self, id):
         """Retrieves a specific meetup"""
-        meetup = [new_meetup for new_meetup in self.db
-                  if new_meetup["id"] == id]
-        return meetup
+        question = [new_question for new_question in self.db
+                    if new_question["id"] == id]
+        return question

f8395cee28b89168af0c6f36eaf16302da656e07
Bakley <barclaykoin@gmail.com>
2019-01-10 07:41:23 +0300

    [features #163046732]add the url path

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api
 from app.api.v1.views.meetups_views import AllMeetupResource, MeetupResource
+from app.api.v1.views.questions_views import QuestionResource
+
 
 version_1 = Blueprint('version1', __name__, url_prefix='/api/v1')
 
@@ -9,3 +11,4 @@ api = Api(version_1)
 
 api.add_resource(MeetupResource, '/meetups', '/meetups/<int:meetup_id>')
 api.add_resource(AllMeetupResource, '/meetups/upcoming')
+api.add_resource(QuestionResource, '/questions')

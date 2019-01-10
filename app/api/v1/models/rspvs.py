d57ae488d514ee7b1177404dd02cf797c2f4f1e0
Bakley <barclaykoin@gmail.com>
2019-01-10 08:29:09 +0300

    [starts #163047100]build the rspv models

---
+++ app/api/v1/models/rspvs.py
@@ -0,0 +1,30 @@
+rspv_models = []
+
+
+class RspvsModel:
+    """rspv class models"""
+
+    def __init__(self):
+        """Initializes the model"""
+        self.db = rspv_models
+
+    def create_rspv(self, meetup, topic, status):
+        """Method to save rspv records"""
+        payload = {
+            "id": len(self.db) + 1,
+            "meetup": meetup,
+            "topic": topic,
+            "status": status,
+        }
+        self.db.append(payload)
+        return payload
+
+    def get_all_rspvs(self):
+        """Retrieves all upcoming rspvs"""
+        return self.db
+
+    def get_a_specific_rspv(self, id):
+        """Retrieves a specific rspv"""
+        rspv = [new_rspv for new_rspv in self.db
+                if new_rspv["id"] == id]
+        return rspv

0cfc65acb2c85819e467dcf2d35607091c4a778b
Bakley <barclaykoin@gmail.com>
2019-01-10 21:30:55 +0300

    [starts #163047182]registerd the upvote url path

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 from app.api.v1.views.meetups_views import AllMeetupResource, MeetupResource
-from app.api.v1.views.questions_views import QuestionResource
+from app.api.v1.views.questions_views import QuestionResource, UpvoteResource
 from app.api.v1.views.rspvs_views import RspvsResource
 
 
@@ -11,6 +11,8 @@ api = Api(version_1)
 
 
 api.add_resource(MeetupResource, '/meetups', '/meetups/upcoming')
-api.add_resource(AllMeetupResource, '/meetups/<int:meetup_id>', '/meetups/<int:meetup_id>/rspv' )
+api.add_resource(AllMeetupResource, '/meetups/<int:meetup_id>',
+                 '/meetups/<int:meetup_id>/rspv')
 api.add_resource(QuestionResource, '/questions')
 api.add_resource(RspvsResource, '/meetups/<int:meetup_id>/rspvs')
+api.add_resource(UpvoteResource, '/questions/<int:questions_id>/upvote')

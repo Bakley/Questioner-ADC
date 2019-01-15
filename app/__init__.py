b15b13b7009f3daf6fb57ecc603fadb883ec65fd
Bakley <barclaykoin@gmail.com>
2019-01-15 16:15:28 +0300

    [chore #163216519]add .env file and configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 """Initialize app."""
 import os
 from flask import Flask
+from dotenv import load_dotenv
 from app.auth.v1 import version_1auth
 from app.api.v1 import version_1
 
@@ -37,3 +38,9 @@ def create_app(config_name):
         return error.get_response()
 
     return app
+
+
+# refers to application_top
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)

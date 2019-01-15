04e8a4a3c2b7ba3cf108bdd4ab2e214521e94c9c
Bakley <barclaykoin@gmail.com>
2019-01-15 14:02:47 +0300

    [chore #163216519]add method to retrive meetup record by location

--- app/api/v1/models/meetups.py
+++ app/api/v1/models/meetups.py
@@ -47,3 +47,11 @@ class MeetupsModel:
             "happeningOn": meetup[0]["happeningOn"],
             "tags": meetup[0]["tags"]
         }]
+
+    def get_a_meetup_by_loaction(self, location):
+        """Retrieves a meetup by its location"""
+        meetup_location = [new_meetup for new_meetup in self.db
+                           if new_meetup["location"] == location]
+        if meetup_location:
+            return True
+        return False

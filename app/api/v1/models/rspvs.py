701214eb348bb048a174fa00abab44542eb60fac
Bakley <barclaykoin@gmail.com>
2019-01-15 15:16:09 +0300

    [chore #163216519]add functionality to locate meetup

--- app/api/v1/models/rspvs.py
+++ app/api/v1/models/rspvs.py
@@ -1,3 +1,4 @@
+from app.api.v1.models.meetups import meetup_models
 rspv_models = []
 
 
@@ -8,11 +9,16 @@ class RspvsModel:
         """Initializes the model"""
         self.db = rspv_models
 
-    def create_rspv(self, meetup, topic, status, createdBy):
+    def create_rspv(self, topic, status, createdBy):
         """Method to save rspv records"""
+
+        meetup = [new_meetup['id'] for new_meetup in meetup_models]
+        if not meetup:
+            return False
+        print(meetup)
         payload = {
             "id": len(self.db) + 1,
-            "meetup": meetup,
+            "meetup": meetup[0],
             "topic": topic,
             "status": status,
             "createdBy": createdBy

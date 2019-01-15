ad3974c7b057a3c8c1b95a30b387590b6d9085fe
Bakley <barclaykoin@gmail.com>
2019-01-15 09:04:40 +0300

    [chore #163141430]add more test to cover the content type

--- tests/v1/test_meetup_view.py
+++ tests/v1/test_meetup_view.py
@@ -17,18 +17,21 @@ class TestMeetupRecord(BaseClassTest):
                                     data=json.dumps(self.meetup),
                                     content_type='application/json')
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.content_type, 'application/json')
 
     def test_get_a_meetup(self):
         """Test if the we can get a specific meetup"""
         response_get = self.client.get('/api/v1/meetups/1',
                                        content_type='application/json')
         self.assertEqual(response_get.status_code, 200)
+        self.assertEqual(response_get.content_type, 'application/json')
 
     def test_get_all_meetup(self):
         """Test if the we can get all meetup records"""
         response_get_all = self.client.get('/api/v1/meetups/upcoming',
                                            content_type='application/json')
         self.assertEqual(response_get_all.status_code, 200)
+        self.assertEqual(response_get_all.content_type, 'application/json')
 
     def test_delete_a_meetup(self):
         """Test if a meetup is deleted"""
@@ -41,3 +44,4 @@ class TestMeetupRecord(BaseClassTest):
         response_delete = self.client.delete('/api/v1/meetups/<int:meetup_id>',
                                              content_type='application/json')
         self.assertEqual(response_delete.status_code, 404)
+        self.assertEqual(response_delete.content_type, 'text/html')

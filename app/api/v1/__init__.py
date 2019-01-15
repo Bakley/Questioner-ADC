623636d24d3a19f0d86fe8d11a4f28507c54c07c
Bakley <barclaykoin@gmail.com>
2019-01-15 12:47:31 +0300

    [chore #163216519]register the url path for getting all rspvs

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,7 +4,7 @@ from app.api.v1.views.meetups_views import AllMeetupResource, MeetupResource
 from app.api.v1.views.questions_views import (QuestionResource,
                                               UpvoteResource,
                                               DownvoteResource)
-from app.api.v1.views.rspvs_views import RspvsResource
+from app.api.v1.views.rspvs_views import RspvsResource, GetRspvs
 
 
 version_1 = Blueprint('version1', __name__, url_prefix='/api/v1')
@@ -16,6 +16,7 @@ api.add_resource(MeetupResource, '/meetups', '/meetups/upcoming')
 api.add_resource(AllMeetupResource, '/meetups/<int:meetup_id>',
                  '/meetups/<int:meetup_id>/rspv')
 api.add_resource(QuestionResource, '/questions')
-api.add_resource(RspvsResource, '/meetups/<int:meetup_id>/rspvs')
 api.add_resource(UpvoteResource, '/questions/<int:questions_id>/upvote')
 api.add_resource(DownvoteResource, '/questions/<int:questions_id>/downvote')
+api.add_resource(RspvsResource, '/meetups/<int:meetup_id>/rspvs', '/rspvs')
+api.add_resource(GetRspvs, '/rspvs')

d28218859ceda9728e2215349c7920e4a3a0e0ba
Bakley <barclaykoin@gmail.com>
2019-01-16 14:05:36 +0300

    [chore #163216519]removed the unused code

--- app/__init__.py
+++ app/__init__.py
@@ -7,21 +7,6 @@ from app.api.v1 import version_1
 
 from config import app_config
 
-from flask import jsonify
-
-
-class ApiError(Exception):
-    def __init__(self, message, payload=None, status_code=400):
-        Exception.__init__(self)
-        self.message = message
-        self.status_code = status_code
-        self.payload = payload or ()
-
-    def get_response(self):
-        ret = dict(self.payload)
-        ret['message'] = self.message
-        return jsonify(ret), self.status_code
-
 
 def create_app(config_name):
     """Create the app with the desired environment."""
@@ -32,10 +17,6 @@ def create_app(config_name):
 
     app.register_blueprint(version_1auth)
     app.register_blueprint(version_1)
-
-    @app.errorhandler(ApiError)
-    def handle_api_error(error):
-        return error.get_response()
 
     return app
 

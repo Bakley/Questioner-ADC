6d3148d9f54e893cce525946b1a77ccc4be9a982
Bakley <barclaykoin@gmail.com>
2019-01-16 14:08:33 +0300

    [chore #163216519]add functionality to check for errors

--- app/api/v1/views/rspvs_views.py
+++ app/api/v1/views/rspvs_views.py
@@ -1,8 +1,9 @@
 from flask_restful import Resource, reqparse
 from app.api.v1.models import rspvs
-from app.utilities.validator_functions import (check_for_empty_string)
+from app.api.v1.models import meetups
 
 rspv_view = rspvs.RspvsModel()
+meetup_rsvp_view = meetups.MeetupsModel()
 
 
 class RspvsResource(Resource):
@@ -10,27 +11,30 @@ class RspvsResource(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('status', required=True,
                         help='status cannot be blank', type=str)
-    parser.add_argument('topic', required=True,
-                        help='topic cannot be blank', type=str)
     parser.add_argument('createdBy', required=True,
                         help='created by cannot be blank', type=int)
 
     def post(self, meetup_id):
         """Method to create an RSPV"""
-        try:
-            args = RspvsResource.parser.parse_args()
-            status = args.get('status')
-            topic = args.get('topic')
-        except Exception:
+
+        # Get a meetup
+
+        meetup = meetup_rsvp_view.get_a_specific_meetup(id=meetup_id)
+        print(meetup)
+        if not meetup:
             return {
-                "status": 400,
-                "error": "Invalid key error"
-            }, 400
+                "status": 404,
+                "error":
+                "No meetup of id {} found to RSPV for".format(meetup_id)
+            }, 404
+
+        args = RspvsResource.parser.parse_args()
+        status = args.get('status')
 
         matchers = ["Yes", "No", "Maybe"]
 
         matching = [
-            value for value in status if any(xs in status for xs in matchers)]
+            value for value in status if any(index in status for index in matchers)]
 
         if not matching:
             return {
@@ -39,20 +43,20 @@ class RspvsResource(Resource):
             }, 400
 
         rspv = rspv_view.create_rspv(status=args.get('status'),
-                                     topic=args.get('topic'),
                                      createdBy=args.get(
             'createdBy')
         )
-        print(rspv)
-        if not rspv:
-            return {
-                "status": 404,
-                "error": "No meetup found to RSPV for"
-            }, 404
+        # Get the required output
+        for values in rspv and meetup:
+            r = [{
+                 "meetup": meetup[0]["id"],
+                 "status": rspv["status"],
+                 "topic": meetup[0]["topic"]
+                 }]
 
         return {
             "status": 201,
-            "data": rspv,
+            "data": r,
             "Message": "rspv successfully created"
         }, 201
 

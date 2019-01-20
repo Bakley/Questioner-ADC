1da6d0a2754e4eff1a1a2dafdd67c9d6a4591cc7
Bakley <barclaykoin@gmail.com>
2019-01-20 18:16:49 +0300

    [finishes #163348434]add test

---
+++ tests/v2/basecases.py
@@ -0,0 +1,37 @@
+"""Base test class"""
+import unittest
+import json
+from app import create_app
+from app.api.v2.dbmodel import QuestionerDb
+
+
+class TestBaseCase(unittest.TestCase):
+    """Base Testing Class."""
+
+    def setup(self):
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        self.client.testing = True
+
+        self.user_one = {
+            "firstname": "Barclay",
+            "lastname": "Koin",
+            "othername": "Sofware Dev",
+            "phoneNumber": "0703912965",
+            "username": "Koin253",
+            "email": "barclay@koin.com",
+            "userpassword": "password"
+        }
+
+        self.user_two = {
+            "firstname": "Derrick",
+            "lastname": "Koin",
+            "othername": "Software Dev",
+            "phoneNumber": "0703912965",
+            "username": "Koin254",
+            "email": "derrick@koin.com",
+            "userpassword": "password"
+        }
+
+    def tearDown(self):
+        QuestionerDb.drop_all()
---
+++ tests/v2/test_user_register.py
@@ -0,0 +1,32 @@
+import unittest
+import json
+from tests.v2.basecases import TestBaseCase
+
+
+class AuthTestCase(TestBaseCase):
+    """
+    test class for the registration endpoint
+    """
+
+    def test_signup(self):
+        """Test if a user can signup"""
+        response2 = self.client.post('/auth/v2/signup',
+                                     data=json.dumps(self.user_one),
+                                     content_type='application/json')
+        self.assertEqual(response2.status_code, 201)
+
+    def test_for_dupicate_registery(self):
+        """Test if a user can register twice"""
+        response1 = self.client.post('/auth/v2/signup',
+                                     data=json.dumps(self.user_two),
+                                     content_type='application/json')
+        self.assertEqual(response1.status_code, 201)
+
+        duplicate_signup = self.client.post('/auth/v2/signup',
+                                            data=json.dumps(self.user_two),
+                                            content_type='application/json')
+        self.assertEqual(duplicate_signup.status_code, 409)
+
+
+if __name__ == "__main__":
+    unittest.main()

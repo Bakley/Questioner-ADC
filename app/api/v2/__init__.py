a5e45eed25b7d3867a73d59d8d8cb1c6a7bea3c3
Bakley <barclaykoin@gmail.com>
2019-01-22 10:23:51 +0300

    [feature #163348428]register the meetup path

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api
-
+from app.api.v2.views.meetups_views import MeetupViewsResource
 
 version_2 = Blueprint('version2', __name__, url_prefix='/api/v2')
 
 api = Api(version_2, catch_all_404s=True)
+api.add_resource(MeetupViewsResource, '/meetups')

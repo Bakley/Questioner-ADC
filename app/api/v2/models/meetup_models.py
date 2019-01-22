6fcc6b09c70ecf0ef1e794a1cc0203a74dad16cf
Bakley <barclaykoin@gmail.com>
2019-01-22 15:39:20 +0300

    [features #163348426]add functionality to get a specific meetup

--- app/api/v2/models/meetup_models.py
+++ app/api/v2/models/meetup_models.py
@@ -23,3 +23,16 @@ class MeetupsModel:
         meetup_tuple = (userid, location, topic, tags, happeningon)
         QuestionerDb.persist_to_db(meetup_query, meetup_tuple)
         return payload
+
+    def get_a_specific_meetup(self, id):
+        """Get one meetup by id"""
+
+        fetch_query = """SELECT * FROM meetups WHERE meetupId={}""".format(
+            id)
+        response = QuestionerDb.retrieve_all(fetch_query)
+        if not response:
+            return False
+        payload = {
+            "one_meetup": response
+        }
+        return payload

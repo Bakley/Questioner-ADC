aa95e92c2fff638b08c080dab20db2eaeb4bc1f9
Bakley <barclaykoin@gmail.com>
2019-01-22 17:49:00 +0300

    [starts #163348425]add path for upcoming url

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 from flask_restful import Api
-from app.api.v2.views.meetups_views import MeetupViewsResource, SpecificMeetup
+from app.api.v2.views.meetups_views import (MeetupViewsResource,
+                                            SpecificMeetup,
+                                            UpcomingMeetups)
 
 version_2 = Blueprint('version2', __name__, url_prefix='/api/v2')
 
 api = Api(version_2, catch_all_404s=True)
 api.add_resource(MeetupViewsResource, '/meetups')
 api.add_resource(SpecificMeetup, '/meetups/<meetup_id>')
+api.add_resource(UpcomingMeetups, '/meetups/upcoming')

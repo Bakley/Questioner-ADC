9661336ebf5b68b3b37ab19303ef36d747259d60
Bakley <barclaykoin@gmail.com>
2019-01-23 16:05:52 +0300

    [finishes #163421648]add URL path to patch method

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -4,7 +4,8 @@ from app.api.v2.views.meetups_views import (MeetupViewsResource,
                                             SpecificMeetup,
                                             UpcomingMeetups)
 
-from app.api.v2.views.question_views import QuestionViewsResource
+from app.api.v2.views.question_views import (QuestionViewsResource,
+                                             UpvoteResource)
 
 version_2 = Blueprint('version2', __name__, url_prefix='/api/v2')
 
@@ -14,3 +15,4 @@ api.add_resource(MeetupViewsResource, '/meetups')
 api.add_resource(SpecificMeetup, '/meetups/<meetup_id>')
 api.add_resource(UpcomingMeetups, '/meetups/upcoming')
 api.add_resource(QuestionViewsResource, '/meetups/<meetup_id>/questions')
+api.add_resource(UpvoteResource, '/questions/<questions_id>/upvote')

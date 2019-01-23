d88ef17c698177390653464a06f9c4d6289dc660
Bakley <barclaykoin@gmail.com>
2019-01-23 16:17:52 +0300

    [feature #163421648]add the upvote method

--- app/api/v2/models/question_model.py
+++ app/api/v2/models/question_model.py
@@ -16,3 +16,19 @@ class QuestionModel:
         question_tuple = (title, body, userId, meetupId)
         response = QuestionerDb.add_to_db(question_query, question_tuple)
         return response
+
+    def upvote_question(self, id):
+        """User can upvote"""
+        upvote_query = """UPDATE questions
+        SET votes = votes + 1
+        WHERE questionid = '{}'""".format(id)
+        QuestionerDb.update_row(upvote_query)
+        fetch_query = """SELECT * FROM questions
+        WHERE questionid = '{}'""".format(id)
+        response = QuestionerDb.retrieve_all(fetch_query)
+        if not response:
+            return False
+        payload = {
+            "question": response
+        }
+        return payload
